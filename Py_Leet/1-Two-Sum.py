19e68ace286a0bb9e5fb8e109be18492df03287f
Akrem Benmbarek <akrem.benmbarek@ensi-uma.tn>
2023-07-31 14:15:07 +0100

    .

--- Py_Leet/1-Two-Sum.py
+++ Py_Leet/1-Two-Sum.py
@@ -1,4 +1,8 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums)!=len(set(nums))
-    #O(n)
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hist = {}
+        for i in range ( len(nums)) :
+            if target - nums[i] in hist:
+               return [i,hist[target - nums[i]]]
+            else : hist[nums[i]] = i
+            #O(n)
