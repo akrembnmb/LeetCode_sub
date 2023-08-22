7af91b90f6efa421f623afd2c70987efe510d695
akrembnmb <benmbarekakrem1@gmail.com>
2023-08-22 18:44:32 +0100

    .

--- Py_Leet/121-Best-Time-to-Buy-and-Sell-Stock.py
+++ Py_Leet/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -8,4 +8,15 @@ class Solution:
             res = max(res,e-lowest)
         return res
         #O(n)
-       
+        # 2 Pointers
+        """l , r = 0,1
+        maxP = 0
+        while r < len (prices) and l<r :
+            if prices[l] < prices[r] :
+                profit = prices[r] - prices[l] 
+                maxP = max(maxP ,profit )
+            else :
+                l=r
+            r+=1
+        return maxP
+        #O(n)"""
