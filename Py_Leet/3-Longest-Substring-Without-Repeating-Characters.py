e3d11a8b59ea9d0697b16eaae51cdfae9726e002
akrembnmb <benmbarekakrem1@gmail.com>
2023-08-23 17:02:55 +0100

    .

---
+++ Py_Leet/3-Longest-Substring-Without-Repeating-Characters.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res = 0
+        hashset = set()
+        l = 0
+        for r in range (len(s)):
+            if s[r] not in hashset :
+                hashset.add(s[r])
+                res = max(res , len(hashset))
+            else :
+                while s[l] != s[r]:
+                    hashset.remove(s[l])
+                    l += 1
+                l += 1  
+        return res
+        
+        #O(n) Time
+    
+        #O(n) Space
