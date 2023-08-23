608c6bc0c16899ce76752c04dad8373f5af0b18e
akrembnmb <benmbarekakrem1@gmail.com>
2023-08-23 17:10:30 +0100

    .

--- Py_Leet/3-Longest-Substring-Without-Repeating-Characters.py
+++ Py_Leet/3-Longest-Substring-Without-Repeating-Characters.py
@@ -14,6 +14,16 @@ class Solution:
                 l += 1  
         return res
         
+        """ res = 0
+        hashset = set()
+        l = 0
+        for r in range(len(s)):
+            while s[r] in hashset:
+                hashset.remove(s[l])
+                l += 1
+            hashset.add(s[r])
+            res = max(res, len(hashset))
+        return res"""
         #O(n) Time
     
         #O(n) Space
